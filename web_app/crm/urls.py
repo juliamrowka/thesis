e884e1ca4d484876067d26d3bdd8d60b71c6dbb2
juliamrowka <julia.mrowka@gmail.com>
2023-11-13 19:20:17 +0100

    Create login user form

--- web_app/crm/urls.py
+++ web_app/crm/urls.py
@@ -4,5 +4,7 @@ from . import views
 # app_name = "crm" #application namespace
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 
 ]
