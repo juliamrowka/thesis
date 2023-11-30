edf533e343d7a1ddc6a3696afb323185957316d1
juliamrowka <julia.mrowka@gmail.com>
2023-11-30 21:05:37 +0100

    Modify templates and add filename to session

--- web_app/experiment/views.py
+++ web_app/experiment/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import DocumentForm
 from .models import Document
-import io
 import openpyxl
-import pandas as pd
+from io import BytesIO
+# import pandas as pd
 # import joblib
 # Create your views here.
 
@@ -15,7 +15,6 @@ def model_form_upload(request):
     show upload form and when the file is uploaded preview this file
     """
     if request.user.is_authenticated:
-        excel_data = list()
         if request.method == 'POST':
             form = DocumentForm(request.POST, request.FILES)
             filename = "documents/user_" + str(request.user.id) + "/" + str(request.FILES['document'])
@@ -25,26 +24,15 @@ def model_form_upload(request):
                 newdoc.user = request.user
                 newdoc.save()
                 messages.success(request, "You have successfully uploaded file!")
-                excel_file = request.FILES['document']
-                print(excel_file)
-                wb = openpyxl.load_workbook(excel_file)
-                worksheet = wb.active
-                print(worksheet)
-                # iterating over the rows and
-                # getting value from each cell in row
-                for row in worksheet.iter_rows():
-                    row_data = list()
-                    for cell in row:
-                        row_data.append(str(cell.value))
-                    excel_data.append(row_data)
-                # return redirect('upload')
+                request.session['filename'] = filename
+                return redirect('upload')
             elif files.count() > 0:
                 messages.success(request, "A file with that name already exists.")
         else:
             form = DocumentForm()
             # return render(request, 'upload.html', {'form': form})
         documents = Document.objects.filter(user=request.user)
-        return render(request, 'upload.html', {'documents': documents, 'form': form, 'excel_data': excel_data})
+        return render(request, 'upload.html', {'documents': documents, 'form': form})
     else:
         messages.success(request, "You need to log in first")
         return redirect('home')
@@ -108,8 +96,18 @@ def model_form_upload(request):
 
 def experiment(request):
       if request.user.is_authenticated:
-        user_name = request.user.first_name
-        return render(request, 'experiment.html', {})
+        excel_data = list()
+        wb = openpyxl.load_workbook(request.session['filename'])
+        worksheet = wb.active
+        print(worksheet)
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+        return render(request, 'experiment.html', {'excel_data': excel_data})
       else:
         messages.success(request, "You need to log in first")
         return redirect('home')
