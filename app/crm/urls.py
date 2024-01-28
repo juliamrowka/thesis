1f7bc60734d5384c2713a1e4ae53fe86dde8d8be
juliamrowka <julia.mrowka@gmail.com>
2024-01-28 12:12:50 +0100

    Rename root directory

--- app/crm/urls.py
+++ app/crm/urls.py
@@ -4,7 +4,6 @@ from . import views
 # app_name = "crm" #application namespace
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
 ]
